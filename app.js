b2619c26998c443c65d05b91dfb34aa4cef6ec6a
nikkifargus <nikki@fargus.co.uk>
2017-10-11 10:04:18 +0100

    got the form errors almost finished

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
+
+
+
 function checkValid(){
+
+
   var inp1 = document.getElementById('inp1');
   var inp2 = document.getElementById('inp2');
-  if(inp1.checkValidity(event) === false){
-    event.preventDefault();
-    // $('#emailError').addClass('error_show');
-    console.log('hit');
+  if(inp1.checkValidity() === false){
 
-  } else if (inp2.checkValidity() === false){
-    document.getElementById('passwordError').innerHTML = 'Password must be at least 7 characters';
+    $('.emailError').show();
+
+  } else if (inp1.checkValidity() === true){
+    $('.emailError').hide();
+  }
+  if(inp2.checkValidity() === false){
+
+    $('.passwordError').show();
+
+  } else if (inp2.checkValidity() === true){
+    $('.passwordError').hide();
   }
 }
 
-
-
-
 // function checkValid(){
+//   var inp1 = document.getElementById('inp1');
+//   var inp2 = document.getElementById('inp2');
+//   if(inp1.checkValidity() === false){
 //
+//     $('.emailError').append('<div class="errorMessage"><img src="images/icon-error.svg"> Not a valid email address</div>');
+//
+//   } else if (inp2.checkValidity() === false){
+//     $('.passwordError').append('<div><img src="images/icon-info.svg"> Password must be at least 7 characters</div>');
+//   }
+// }
+
+
+// function checkValid(event){
+//   // event.preventDefault();
 //   const email = document.registerForm.email.value;
 //   const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 //   const password = document.registerForm.password.value;
@@ -28,4 +49,9 @@ function checkValid(){
 //     $('.passwordError').append('<div><img src="images/icon-info.svg">Password must be at least 7 characters</div>');
 //     return false;
 //   }
+// }
+
+// function append(){
+//   console.log('clicked');
+//     $('.passwordError').append('<div><img src="images/icon-error.svg"> Not a valid email address</div>');
 // }
