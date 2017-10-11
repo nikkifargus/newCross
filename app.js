2c924cc1fb9706a7957691499cb2ef59adf794aa
nikkifargus <nikki@fargus.co.uk>
2017-10-11 10:26:03 +0100

    can get error messages to show but built in errors also show

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
 function checkValid(){
 
 
@@ -22,7 +20,36 @@ function checkValid(){
   }
 }
 
+//TRIAL WITHOUT USING BUILT IN VALIDATOR
+
 // function checkValid(){
+//   // event.preventDefault();
+//   $('form').submit(function(e){
+//     e.preventDefault();
+//   });
+//
+//   const email = document.form.email.value;
+//   const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+//   const password = document.form.password.value;
+//
+//   if(email === null || email === '' || reg.test(email)=== false){
+//     console.log('email');
+//     $('.emailError').show();
+//   } else if (reg.test(email)=== true){
+//     $('.emailError').hide();
+//     return true;
+//   } else if(password.length < 7){
+//     $('.passwordError').show();
+//   } else if(password.length > 7){
+//     $('.passwordError').hide();
+//   }
+// }
+
+//INITIAL TRIAL FOR ERROR MESSAGES
+// function checkValid(){
+//
+//
+//
 //   var inp1 = document.getElementById('inp1');
 //   var inp2 = document.getElementById('inp2');
 //   if(inp1.checkValidity() === false){
@@ -34,24 +61,8 @@ function checkValid(){
 //   }
 // }
 
-
-// function checkValid(event){
-//   // event.preventDefault();
-//   const email = document.registerForm.email.value;
-//   const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-//   const password = document.registerForm.password.value;
-//
-//   if(email === null || email === '' || reg.test(email)=== false){
-//     console.log('email');
-//     $('.emailError').append('<div><img src="images/icon-error.svg"> Not a valid email address</div>');
-//     return false;
-//   }else if(password.length<7){
-//     $('.passwordError').append('<div><img src="images/icon-info.svg">Password must be at least 7 characters</div>');
-//     return false;
-//   }
-// }
-
+//TESTING APPEND WORKING
 // function append(){
 //   console.log('clicked');
-//     $('.passwordError').append('<div><img src="images/icon-error.svg"> Not a valid email address</div>');
+//   $('.passwordError').show();
 // }
